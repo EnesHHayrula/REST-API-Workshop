610e121df8d76eadd0fc12b4ca3db0f4b72f7445
EnesHHayrula <eneshayrula@gmail.com>
2023-10-19 21:12:25 +0300

    edit functionality implemented

--- server/src/services/furnitureService.js
+++ server/src/services/furnitureService.js
@@ -3,3 +3,8 @@ const Furniture = require("../models/Furniture");
 exports.getAll = () => Furniture.find();
 
 exports.create = (furnitureData) => Furniture.create(furnitureData);
+
+exports.getOne = (furnitureId) => Furniture.findById(furnitureId);
+
+exports.update = (furnitureId, furnitureData) =>
+  Furniture.findByIdAndUpdate(furnitureId, furnitureData);
